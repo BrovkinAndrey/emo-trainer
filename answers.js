87a1d76498185a8768eb1483cbe5eacaa5086539
Andrey <brovkin_58@mail.ru>
2026-09-16 19:20:53 +0300

    add linter ESlint @stylistic/eslint-plugin

--- answers.js
+++ answers.js
@@ -1,7 +1,7 @@
-const container = document.querySelector(".container");
-const restartBtn = document.querySelector(".restart-btn");
+const container = document.querySelector('.container')
+const restartBtn = document.querySelector('.restart-btn')
 
-const data = JSON.parse(localStorage.getItem("quizResults"));
+const data = JSON.parse(localStorage.getItem('quizResults'))
 
 const resultIcons = {
   right: `<svg class="result-icon" width="73" height="68" viewBox="0 0 73 68" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -10,35 +10,37 @@ const resultIcons = {
   wrong: `<svg class="result-icon" width="71" height="72" viewBox="0 0 71 72" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M18.9595 36.7533L1.77463 54.8671C1.40157 55.2603 1.40971 55.8792 1.79299 56.2625L14.8788 69.3483C15.2764 69.7459 15.9236 69.7376 16.3109 69.3299L33.9063 50.8084C34.2902 50.4043 34.9306 50.3921 35.3296 50.7814L54.3972 69.384C54.7891 69.7663 55.4156 69.7624 55.8027 69.3753L68.8971 56.2808C69.2877 55.8903 69.2877 55.2571 68.8971 54.8666L50.8025 36.7719C50.4154 36.3849 50.4115 35.7586 50.7936 35.3667L68.9265 16.7688C69.304 16.3817 69.3053 15.7645 68.9295 15.3757L55.8111 1.80501C55.4227 1.40316 54.7803 1.39771 54.3851 1.79292L35.3114 20.8666C34.9208 21.2571 34.2877 21.2571 33.8971 20.8666L16.2866 3.25603C15.9058 2.87533 15.2921 2.86438 14.898 3.23125L1.83377 15.3945C1.43049 15.77 1.40693 16.401 1.78109 16.8055L18.9681 35.386C19.326 35.7729 19.3222 36.371 18.9595 36.7533Z" fill="#C30000" stroke="black" stroke-width="3"/>
 </svg>`,
-};
+}
 
-const colorizeVowels = (text) =>
-  text.replace(/[аеёиоуЫэюя]/gi, (v) => `<span class="vowel">${v}</span>`);
+const x = 'текст'
 
-const table = document.createElement("table");
-table.classList.add("result-table");
+const colorizeVowels = text =>
+  text.replace(/[аеёиоуЫэюя]/gi, v => `<span class="vowel">${v}</span>`)
 
-const thead = document.createElement("thead");
+const table = document.createElement('table')
+table.classList.add('result-table')
 
-const tr = document.createElement("tr");
+const thead = document.createElement('thead')
+
+const tr = document.createElement('tr')
 tr.innerHTML = `
   <th>#</th>
-  <th>${colorizeVowels("КАРТИНКА")}</th>
-  <th>${colorizeVowels("ПРАВИЛЬНЫЙ ОТВЕТ")}</th>
-  <th>${colorizeVowels("ТВОЙ ОТВЕТ")}</th>
-  <th>${colorizeVowels("РЕЗУЛЬТАТ")}</th>
-`;
+  <th>${colorizeVowels('КАРТИНКА')}</th>
+  <th>${colorizeVowels('ПРАВИЛЬНЫЙ ОТВЕТ')}</th>
+  <th>${colorizeVowels('ТВОЙ ОТВЕТ')}</th>
+  <th>${colorizeVowels('РЕЗУЛЬТАТ')}</th>
+`
 
-const tbody = document.createElement("tbody");
+const tbody = document.createElement('tbody')
 
 data.forEach((row, i) => {
-  const tr = document.createElement("tr");
-  const taskWord = colorizeVowels(row.taskWord.toUpperCase());
-  const userWord = colorizeVowels(row.userWord.toUpperCase());
-  const result =
-    row.taskWord.toUpperCase() === row.userWord.toUpperCase()
-      ? "right"
-      : "wrong";
+  const tr = document.createElement('tr')
+  const taskWord = colorizeVowels(row.taskWord.toUpperCase())
+  const userWord = colorizeVowels(row.userWord.toUpperCase())
+  const result
+    = row.taskWord.toUpperCase() === row.userWord.toUpperCase()
+      ? 'right'
+      : 'wrong'
 
   tr.innerHTML = `
     <td>${i + 1}</td>
@@ -46,17 +48,17 @@ data.forEach((row, i) => {
     <td>${taskWord}</td>
     <td>${userWord}</td>
     <td>${resultIcons[result]}</td>
-  `;
-  tbody.appendChild(tr);
-});
+  `
+  tbody.appendChild(tr)
+})
 
-thead.appendChild(tr);
-table.appendChild(thead);
-table.appendChild(tbody);
-container.appendChild(table);
+thead.appendChild(tr)
+table.appendChild(thead)
+table.appendChild(tbody)
+container.appendChild(table)
 
-restartBtn.addEventListener("click", () => {
-  localStorage.removeItem("quizResults");
+restartBtn.addEventListener('click', () => {
+  localStorage.removeItem('quizResults')
 
-  window.location.href = "index.html";
-});
+  window.location.href = 'index.html'
+})
--- script.js
+++ script.js
@@ -1,122 +1,122 @@
-const answerForm = document.querySelector(".answer-form");
-const input = document.getElementById("task-input");
-const submitBtn = document.querySelector(".submit-task");
-const taskImage = document.getElementById("task-image");
-const taskWord = document.getElementById("task-word");
-const pageBtns = document.querySelectorAll(".btn-page");
-const restartBtn = document.querySelector(".restart-btn");
+const answerForm = document.querySelector('.answer-form')
+const input = document.getElementById('task-input')
+const submitBtn = document.querySelector('.submit-task')
+const taskImage = document.getElementById('task-image')
+const taskWord = document.getElementById('task-word')
+const pageBtns = document.querySelectorAll('.btn-page')
+const restartBtn = document.querySelector('.restart-btn')
 
 const originalData = [
-  { img: "./images/1.png", taskWord: "Зелень", userWord: "" },
-  { img: "./images/2.png", taskWord: "Ашан", userWord: "" },
-  { img: "./images/3.png", taskWord: "Тюмень", userWord: "" },
-  { img: "./images/4.png", taskWord: "Каток", userWord: "" },
-  { img: "./images/5.png", taskWord: "Каток", userWord: "" },
-];
+  { img: './images/1.png', taskWord: 'Зелень', userWord: '' },
+  { img: './images/2.png', taskWord: 'Ашан', userWord: '' },
+  { img: './images/3.png', taskWord: 'Тюмень', userWord: '' },
+  { img: './images/4.png', taskWord: 'Каток', userWord: '' },
+  { img: './images/5.png', taskWord: 'Каток', userWord: '' },
+]
 
 function shuffle(array) {
-  const arr = [...array];
+  const arr = [...array]
 
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
 
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+    [arr[i], arr[j]] = [arr[j], arr[i]]
   }
 
-  return arr;
+  return arr
 }
 
-let data = shuffle(originalData);
+let data = shuffle(originalData)
 
-let currentIndex = 0;
-const countQuestion = data.length;
-renderQuestion();
+let currentIndex = 0
+const countQuestion = data.length
+renderQuestion()
 
 function updateButtonState() {
-  const isEmpty = input.value.trim() === "";
-  submitBtn.disabled = isEmpty;
+  const isEmpty = input.value.trim() === ''
+  submitBtn.disabled = isEmpty
 }
 
-input.addEventListener("input", updateButtonState);
-input.addEventListener("change", updateButtonState);
+input.addEventListener('input', updateButtonState)
+input.addEventListener('change', updateButtonState)
 
 function saveAnswer() {
-  data[currentIndex].userWord = input.value.trim();
+  data[currentIndex].userWord = input.value.trim()
 }
 
 function renderQuestion() {
   if (currentIndex >= countQuestion) {
-    console.table(data);
-    return;
+    console.table(data)
+    return
   }
 
-  const currentTask = data[currentIndex];
+  const currentTask = data[currentIndex]
 
-  taskImage.src = currentTask.img;
-  taskImage.alt = currentTask.taskWord;
+  taskImage.src = currentTask.img
+  taskImage.alt = currentTask.taskWord
 
-  taskWord.textContent = currentTask.taskWord;
+  taskWord.textContent = currentTask.taskWord
 
-  pageBtns[currentIndex].classList.add("btn-active");
+  pageBtns[currentIndex].classList.add('btn-active')
 
-  input.value = "";
-  input.focus();
+  input.value = ''
+  input.focus()
 
-  updateButtonState();
+  updateButtonState()
 }
 
 function showResults() {
-  localStorage.setItem("quizResults", JSON.stringify(data));
+  localStorage.setItem('quizResults', JSON.stringify(data))
 
-  window.location.href = "answers.html";
+  window.location.href = 'answers.html'
 }
 
 function nextNotAnswered() {
   currentIndex = [...pageBtns].findIndex(
-    (btn) => !btn.classList.contains("btn-answered"),
-  );
+    btn => !btn.classList.contains('btn-answered'),
+  )
   if (currentIndex >= countQuestion) {
-    showResults();
+    showResults()
   }
 }
 
-answerForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (submitBtn.disabled) return;
+answerForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if (submitBtn.disabled) return
 
-  saveAnswer();
-  pageBtns[currentIndex].classList.remove("btn-active");
-  pageBtns[currentIndex].classList.add("btn-answered");
+  saveAnswer()
+  pageBtns[currentIndex].classList.remove('btn-active')
+  pageBtns[currentIndex].classList.add('btn-answered')
 
-  nextNotAnswered();
-  renderQuestion();
-});
+  nextNotAnswered()
+  renderQuestion()
+})
 
 pageBtns.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
-    pageBtns[currentIndex].classList.remove("btn-active");
-    currentIndex = i;
+  btn.addEventListener('click', () => {
+    pageBtns[currentIndex].classList.remove('btn-active')
+    currentIndex = i
     if (currentIndex >= countQuestion) {
-      showResults();
+      showResults()
     }
-    renderQuestion();
-  });
-});
+    renderQuestion()
+  })
+})
 
 function restartTest() {
   data.forEach((item) => {
-    item.user = "";
-  });
+    item.user = ''
+  })
 
-  currentIndex = 0;
+  currentIndex = 0
 
   pageBtns.forEach((btn) => {
-    btn.classList.remove("btn-answered");
-    btn.classList.remove("btn-active");
-  });
+    btn.classList.remove('btn-answered')
+    btn.classList.remove('btn-active')
+  })
 
-  data = shuffle(originalData);
-  renderQuestion();
+  data = shuffle(originalData)
+  renderQuestion()
 }
 
-restartBtn.addEventListener("click", restartTest);
+restartBtn.addEventListener('click', restartTest)
